0c9ffeda965bbe15c488abfd01c35a04b2ef5ae2
Bruce Allan Makaaru <brucemakallan@MacBook-Pro-6.local>
2018-11-22 09:12:40 +0300

    Initial Setup - Create a React Application - Add basic styling and bootstrap - Install redux for state management - Insatll React Router for routing

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import * as serviceWorker from "./serviceWorker";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import "bootstrap/dist/css/bootstrap.css";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const initialState = {};
+const reducer = (state = initialState, action) => {
+  return state;
+};
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+const store = createStore(reducer);
+const App = () => (
+  <Provider store={store}>
+    <React.Fragment>
+      <div>StackOverflow-Lite</div>
+    </React.Fragment>
+  </Provider>
+);
+
+ReactDOM.render(<App />, document.getElementById("root"));
 serviceWorker.unregister();
